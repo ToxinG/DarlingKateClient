b7b9936b102e983d4cf6055cc68f8623f5b70150
Антон <toxan.m@gmail.com>
2016-12-19 23:29:47 +0300

    Added createIntent to NewsFeedActivity, fixes

--- app/src/main/java/ru/luvas/dk/client/NewsFeedActivity.java
+++ app/src/main/java/ru/luvas/dk/client/NewsFeedActivity.java
@@ -1,6 +1,10 @@
 package ru.luvas.dk.client;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +12,11 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.luvas.dk.client.model.News;
+import ru.luvas.dk.client.utils.NewsRecyclerAdapter;
 import ru.luvas.dk.client.utils.RecylcerDividersDecorator;
 
 /**
@@ -16,10 +25,22 @@ import ru.luvas.dk.client.utils.RecylcerDividersDecorator;
 
 public class NewsFeedActivity extends AppCompatActivity {
 
+    private static final String EXTRA_LIST_OF_NEWS = "list_of_news";
+
     private RecyclerView recyclerView;
     private TextView errorTextView;
     private ProgressBar progressBar;
 
+    @Nullable
+    private NewsRecyclerAdapter adapter;
+
+    public static Intent createIntent(@NonNull Context context,
+                                      @NonNull List<News> listOfNews) {
+        final Intent intent = new Intent(context, NewsFeedActivity.class);
+        listOfNews = new ArrayList<>();
+        intent.putParcelableArrayListExtra(EXTRA_LIST_OF_NEWS, (ArrayList<News>) listOfNews);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +66,6 @@ public class NewsFeedActivity extends AppCompatActivity {
     }
 
     /*TODO
-     * Data loading
      * Error notification
-     * Endless scrolling */
+     */
 }
